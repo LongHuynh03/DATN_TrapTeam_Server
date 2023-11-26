097f67cfbfec31340e2cb54e3c2dc08fe5e6b7f0
PLong2003 <99625263+PLong2003@users.noreply.github.com>
2023-11-26 14:19:55 +0700

    Update 26/11

--- routes/index.js
+++ routes/index.js
@@ -24,6 +24,10 @@ router.get('/detailTour', function(req, res, next) {
   res.render('tour/edit', { title: 'Express' });
 });
 
+router.get('/addTour', function(req, res, next) {
+  res.render('tour/add', { title: 'Express' });
+});
+
 router.get('/test', function(req, res, next) {
   res.render('test', { title: 'Express' });
 });
@@ -32,8 +36,12 @@ router.get('/login', function(req, res, next) {
   res.render('login', { title: 'Express' });
 });
 
-router.get('/banner', function(req, res, next) {
-  res.render('banner', { title: 'Express' });
+router.get('/event', function(req, res, next) {
+  res.render('event/list', { title: 'Express' });
+});
+
+router.get('/addEvent', function(req, res, next) {
+  res.render('event/add', { title: 'Express' });
 });
 
 router.get('/bill', function(req, res, next) {
