66fd6df26e4a9158a3f5bbb3b29d3a52080b4fb3
luvantrong <102342647+luvantrong@users.noreply.github.com>
2023-12-05 22:16:31 +0700

    Update BookingTourModel.js

--- components/bookingtour/BookingTourModel.js
+++ components/bookingtour/BookingTourModel.js
@@ -3,23 +3,24 @@ const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
 const bookingtourSchema = new Schema({
-    bookingtour_id: ObjectId,
-    user_id: { type: ObjectId, ref: "user" },
-    tour_id: { type: ObjectId, ref: "tour" },
-    discount: Number,
-    created_at: Date,
-    adult_count: Number,
-    child_count: Number,
-    price: Number,
-    note: String,
-    status: Boolean,
-    role: Boolean,
-    location_custom: [
-        {
-            type: ObjectId,
-            ref: "location",
-        }
-    ],
+  bookingtour_id: ObjectId,
+  user_id: { type: ObjectId, ref: "user" },
+  tour_id: { type: ObjectId, ref: "tour" },
+  discount: Number,
+  created_at: Date,
+  adult_count: Number,
+  child_count: Number,
+  price: Number,
+  note: String,
+  role: Boolean,
+  location_custom: [
+    {
+      type: ObjectId,
+      ref: "location",
+    },
+  ],
 });
 
-module.exports = mongoose.models.bookingtour || mongoose.model("bookingtour", bookingtourSchema);
+module.exports =
+  mongoose.models.bookingtour ||
+  mongoose.model("bookingtour", bookingtourSchema);
