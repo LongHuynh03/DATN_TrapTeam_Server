96a407266b11d415e6e49d67c59df8f1cd1b64cb
luvantrong <102342647+luvantrong@users.noreply.github.com>
2023-12-17 11:36:12 +0700

    Update TourService.js

--- components/tour/TourService.js
+++ components/tour/TourService.js
@@ -61,6 +61,7 @@ const getTourByFilter = async (
   dayFind
 ) => {
   try {
+    const date = new Date(dayFind.split("/").reverse().join("-"))
     const tours = await tourModel.aggregate([
       {
         $lookup: {
@@ -75,7 +76,7 @@ const getTourByFilter = async (
           "province.name": { $regex: locationProvinces, $options: "i" },
           is_popular: is_popular === "true" ? true : false,
           price: { $gte: Number(minPrice), $lte: Number(maxPrice) },
-          departure_date: { $eq: new Date(dayFind) } 
+          departure_date: { $eq: date } 
         },
         },
       
