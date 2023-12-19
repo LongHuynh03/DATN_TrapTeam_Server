d30f5947eb39c5e278b35ac59e274ea5964f7501
luvantrong <102342647+luvantrong@users.noreply.github.com>
2023-12-19 14:43:08 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,56 @@ const adminController = require("../components/admin/AdminController");
 //   res.render("index", { title: "Express" });
 // });
 
+const getPriceByMonth = async () => {
+  try {
+	const month = new Date().getMonth() + 1;
+	const year = new Date().getFullYear();
+	let day = 31;
+  let listPrice = [];
+	switch (month) {
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 8:
+		case 10:
+		case 12:
+			day = 31;
+			break;
+		case 2:
+			if ((year % 4 === 0 && year % 100 !== 0 && year % 400 !== 0) || (year % 100 === 0 && year % 400 === 0)) {
+				day = 29;
+			}
+			else {
+				day = 28;
+			}
+			break;
+		case 4:
+		case 6:
+		case 9:
+		case 11:
+			day = 30;
+			break;
+		default:
+			day = 31;
+			break;
+	}
+	for (let index = 1; index <= day; index++) {
+		const bookings = await bookingTourController.getAllBookingToursByDay(index, month, year);
+		let totalPrice = 0;
+		const total = await bookings.forEach(element => {
+			totalPrice += element.price
+		});
+		listPrice.push(totalPrice);
+	}
+  // console.log(listPrice);
+  return listPrice;
+  } catch (error) {
+    console.log("Get price by month error: " + error);
+    throw error;
+  }
+}
+
 router.get("/login",[auth.authenWeb], function (req, res, next) {
   res.render("login", { title: "Express" });
 });
@@ -48,6 +98,7 @@ router.get("/test", function (req, res, next) {
 
 router.get('/',[auth.authenWeb],async function (req, res, next) {
   try {
+    const chartPrice = await getPriceByMonth();
     const bookings = await bookingTourController.getAllBookingTours();
     let totalPrice = 0;
     const total = bookings.forEach(element => {
@@ -76,7 +127,7 @@ router.get('/',[auth.authenWeb],async function (req, res, next) {
       }
     });
 
-    res.render('index', { newTotalPrice, numberAccounts, numberTours, numberToursActive, numberToursFinish });
+    res.render('index', { newTotalPrice, numberAccounts, numberTours, numberToursActive, numberToursFinish , chartPrice});
   } catch (error) {
     console.log("Get all user cpanel error: " + error)
     throw error;
